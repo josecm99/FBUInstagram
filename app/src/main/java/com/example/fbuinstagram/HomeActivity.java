8491d0e4393c4a9d16722ca043de461b42a16ae1
Jose Cruz Mendoza <josecm99@hotmail.com>
2019-07-09 12:47:09 -0700

    Implemented the photo taking and posting to Parse backend by including a new Activity that shows the image along with an EditText for entering a description.

--- app/src/main/java/com/example/fbuinstagram/HomeActivity.java
+++ app/src/main/java/com/example/fbuinstagram/HomeActivity.java
@@ -1,11 +1,19 @@
 package com.example.fbuinstagram;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.fbuinstagram.models.Post;
 import com.parse.FindCallback;
@@ -14,14 +22,27 @@ import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String IMAGE_PATH = "~/Desktop/ConductorDog.jpeg";
+    private static final String APP_TAG = "HomeActivity";
+    public static final int WIDTH = 250;
     private Button btnCreate;
     private Button btnRefresh;
     private Button btnLogout;
+
+    //Used for opening up the Image Capture Intent
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public final static int POST_CREATION_REQUEST_CODE = 123;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
 
 
     @Override
@@ -37,6 +58,10 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO MAKE SURE TO CHANGE THIS ALL LATER
+
+                Log.d(APP_TAG, "About to launch camera");
+                onLaunchCamera();
+
 //                final String description = etDescription.getText().toString();
 //                final ParseUser currUser = ParseUser.getCurrentUser();
 //
@@ -72,17 +97,148 @@ public class HomeActivity extends AppCompatActivity {
         //Modify the ActionBar
         ActionBar actionBar = getSupportActionBar();
 
-        //Hide title
         getSupportActionBar().setTitle("");
-        //Allows icon to be added
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setLogo(R.drawable.icon);
-        actionBar.setDisplayUseLogoEnabled(true);
 
-
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.nav_logo_whiteout);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
 
         loadTopPosts();
     }// end onCreate
+
+
+
+    public void onLaunchCamera(){
+        // create Intent to take a picture and return control to the calling application
+        Log.d(APP_TAG, "Creating camera intent");
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFile(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        Log.d(APP_TAG, "Put stuff in Intent (FileProvider)");
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.d(APP_TAG, "Am inside onActivityResult");
+
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+
+                Log.d(APP_TAG, "Result was OK and we should have gotten an image");
+
+                Log.d(APP_TAG, "Boutta blast an intent");
+                Intent postCreationIntent = new Intent(this, PostCreationActivity.class);
+
+                postCreationIntent.putExtra("URI", photoFile.getAbsolutePath() );
+
+                startActivityForResult(postCreationIntent, POST_CREATION_REQUEST_CODE);
+
+
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+
+        }// end if check
+
+        else if (requestCode == POST_CREATION_REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+
+                Log.d(APP_TAG, "Trying to create a POST here :))");
+
+                //Redoing this in this Activity so that we can save the resized version for later use
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath() );
+
+                // See BitmapScaler.java: https://gist.github.com/nesquena/3885707fd3773c09f1bb
+                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(takenImage, WIDTH);
+
+
+                // Configure byte output stream
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+// Compress the image further
+                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+// Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+                File resizedFile = getPhotoFile(photoFileName + "_resized");
+                try {
+                    resizedFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(resizedFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+// Write the bytes of the bitmap to file
+                try {
+                    fos.write(bytes.toByteArray());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //Once all the above code has run, we can use the new 'resizedFile' to create our post
+
+
+                //Will now use createPost to actually put the Post on our Parse backend
+                String caption = data.getExtras().getString("caption");
+
+                //Also make sure to get the current user
+                ParseUser user = ParseUser.getCurrentUser();
+
+                //And turn the File into a ParseFile
+                ParseFile parseFile = new ParseFile(resizedFile);
+                createPost(caption, parseFile, user);
+
+            }
+//            else if (resultCode == RESULT_CANCELED){
+//
+//            }
+
+        }// end other if check
+
+    }// end onActivityResult
+
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFile(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
 
 
     private void createPost(String description, ParseFile imageFile, ParseUser user){
@@ -96,9 +252,9 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void done(ParseException e) {
                 if (e == null){
-                    Log.d("HomeActivity", "Created Post successfully! :)");
+                    Log.d(APP_TAG, "Created Post successfully! :)");
                 }else{
-                    Log.e("HomeActivity","Post creation had a booboo :(");
+                    Log.e(APP_TAG,"Post creation had a booboo :(");
                     e.printStackTrace();
                 }
             }
@@ -120,7 +276,7 @@ public class HomeActivity extends AppCompatActivity {
                     for(int i =0; i < objects.size(); i++) {
                         Log.d("HomeActivity", "Post ID: "
                                 + objects.get(i).getDescription()
-                                + "\nUsername: " + objects.get(i).getUser().getUsername() );
+                                + "\tUsername: " + objects.get(i).getUser().getUsername() );
                     }// end for
 
                 }
---
+++ app/src/main/java/com/example/fbuinstagram/PostCreationActivity.java
@@ -0,0 +1,80 @@
+package com.example.fbuinstagram;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static com.example.fbuinstagram.HomeActivity.WIDTH;
+
+public class PostCreationActivity extends AppCompatActivity {
+
+
+    private ImageView ivImage;
+    private EditText etDescription;
+    private Button btnPost;
+    private static final String APP_TAG = "PostCreationActivity";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_creation);
+
+
+        Log.d(APP_TAG, "Created the activity");
+
+
+        //Initialize the layout elements
+        ivImage = (ImageView) findViewById(R.id.ivImage);
+        etDescription = (EditText) findViewById(R.id.etDescription);
+        btnPost = (Button) findViewById(R.id.btnPost);
+
+
+        String photoFilePath = getIntent().getExtras().getString("URI");
+
+        // by this point we have the camera photo on disk
+        Bitmap takenImage = BitmapFactory.decodeFile(photoFilePath);
+
+        // See BitmapScaler.java: https://gist.github.com/nesquena/3885707fd3773c09f1bb
+        Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(takenImage, WIDTH);
+
+
+        ivImage.setImageBitmap(resizedBitmap);
+
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String caption = etDescription.getText().toString();
+
+                if (caption.matches("")){
+                    Toast.makeText(PostCreationActivity.this, "Please add a caption", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Intent result = new Intent();
+
+                    result.putExtra("caption", caption);
+                    setResult(RESULT_OK, result);
+                    finish();
+                }
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
